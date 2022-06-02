26996876f148c19e34b8b6b724c305da37ec5849
Roman Budai <roman1985buday@gmail.com>
2022-06-02 15:24:39 +0300

    1st homework

---
+++ Homework1/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//let varName = +prompt("Введите значение Name", "");
+//let varName = prompt("Введите значение Name", "");
+const varName = 'строка';
+
+typeof varName === 'string' ? 
+console.log('Привет ' + varName) : 
+console.log('Ошибка, не тот тип данных');
+
+console.log(typeof 9);
+console.log(typeof 'string');
+console.log(typeof true);
+console.log(typeof 12 / 0);
+console.log(typeof string);
+console.log(typeof {});
+console.log(typeof 1n);
+
+
+
+
