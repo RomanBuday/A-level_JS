b0fa263fa7123f3db6b623deb1934e12eb9eab76
Roman Budai <roman1985buday@gmail.com>
2022-06-21 18:16:26 +0300

    without 2-nd

--- Homework5/script.js
+++ Homework5/script.js
@@ -23,7 +23,7 @@ const obj = {
 obj.getKeySum = function() {
     let out = 0;
     Object.values(this).forEach((item)=> {
-        if(item ==  true) {
+        if(item == true) {
             out += item;
         }
     });
@@ -31,7 +31,49 @@ obj.getKeySum = function() {
 };
 
 obj.reverseKey = function() {
-    
+    const result = {};
+    Object.entries(obj).forEach(([key, value]) => {
+      result[value] = key;
+    });
 };
 
-console.log(obj);
+console.log(obj.getKeySum());
+console.log(obj.reverseKey());
+
+
+//---------------------------------------------------------
+
+let holderDiv = document.querySelector("div.holder");
+holderDiv.setAttribute("style", "margin: 10px");
+
+
+var item1 = document.createElement("div");
+var itemText = document.createTextNode("1");
+item1.appendChild(itemText);
+item1.className = 'item';
+holderDiv.appendChild(item1);
+//item1.setAttribute("style", "margin-top: 30px");
+
+var item2 = document.createElement("div");
+var itemText = document.createTextNode("2");
+item2.appendChild(itemText);
+item2.className = 'item';
+holderDiv.appendChild(item2);
+
+var item3 = document.createElement("div");
+var itemText = document.createTextNode("3");
+item3.appendChild(itemText);
+item3.className = 'item';
+holderDiv.appendChild(item3);
+
+var item4 = document.createElement("div");
+var itemText = document.createTextNode("4");
+item4.appendChild(itemText);
+item4.className = 'item';
+holderDiv.appendChild(item4);
+
+var item5 = document.createElement("div");
+var itemText = document.createTextNode("5");
+item5.appendChild(itemText);
+item5.className = 'item';
+holderDiv.appendChild(item5);
