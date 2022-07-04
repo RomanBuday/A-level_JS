74311cdd42b7b9e76135ca62163e0a7a50b8ef03
Roman Budai <roman1985buday@gmail.com>
2022-07-04 19:08:10 +0300

    checkwin

--- Homework8/script.js
+++ Homework8/script.js
@@ -1,1 +1,59 @@
 "Use strict";
+
+let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    area =  document.querySelector(".area"),
+    reset = document.querySelector(".reset"),
+    fields = document.querySelectorAll(".field");
+
+
+for (let i = 0; i < 9; i++) {
+    area.innerHTML += '<div class="field"></div>';
+}     
+
+for(let i = 0; i < fields.length; i++) {
+    fields[i].index = i;
+    fields[i].addEventListener('click', function(event) {
+    fields[event.target.index].innerHTML = 'X';
+    delete arr[(arr.indexOf(event.target.index))];
+
+    function compareRandom(a, b) {
+        return Math.random() - 0.5;
+    }
+
+    arr.sort(compareRandom);
+    fields[arr[0]].innerHTML = 'O';
+    checkWin();
+    arr.shift();     
+    });      
+}
+
+function checkWin() {
+    const winArr = [
+        [0, 1, 2],
+        [0, 3 ,6],
+        [0, 4, 8],
+        [1, 4, 7],
+        [2, 5, 8], 
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8],
+    ];    
+
+    for (let j = 0; j < winArr.length; j++) {
+        let win = winArr[j];
+
+        if (fields[win[0]].innerHTML == fields[win[1]].innerHTML &&
+            fields[win[1]].innerHTML == fields[win[2]].innerHTML && 
+            fields[win[0]].innerHTML != '') 
+            {
+            alert(fields[win[0]].innerHTML + ' WIN!');
+        }
+    }
+} 
+
+reset.addEventListener('click', function reset() {
+    for(let i = 0; i < fields.length; i++) {
+        fields[i].innerHTML = '';
+    }
+});
+ 
