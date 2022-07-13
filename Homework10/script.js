b1e5e48203c63d85a1ddf6c4fac69c6a4cec0bff
Roman Budai <roman1985buday@gmail.com>
2022-07-13 15:08:56 +0300

    +counter

--- Homework10/script.js
+++ Homework10/script.js
@@ -1,23 +1,25 @@
 "Use strict";
 
 let userName = document.querySelector(".user__name"),
-    userAge = document.querySelector(".user__age"),
-    loginButton = document.querySelector(".login__btn");
+  userAge = document.querySelector(".user__age"),
+  loginButton = document.querySelector(".login__btn");
 
-document.querySelector('.user__age').addEventListener('input', 
-    function(e){
-      this.value = this.value.replace(/[^\d.]/g, '');
-    }
-);
+document.querySelector(".user__age").addEventListener("input", function (e) {
+  this.value = this.value.replace(/[^\d.]/g, "");
+});
 
 loginButton.addEventListener("click", () => {
-    if(userName.value.length !== 0 && userName.value.trim() !== '' &&
-    userAge.value.length !== 0 && userAge.value.trim() !== '' ||
-    document.cookie.userName !== userName.value && document.cookie.userAge !== userAge.value) {
-        
-        document.cookie = "user" + "=" + (userName.value);
-        document.cookie = "age" + "=" + (userAge.value);
-    }
+  if (
+    (userName.value.length !== 0 &&
+      userName.value.trim() !== "" &&
+      userAge.value.length !== 0 &&
+      userAge.value.trim() !== "") ||
+    (document.cookie.userName !== userName.value &&
+      document.cookie.userAge !== userAge.value)
+  ) {
+    document.cookie = "user" + "=" + userName.value;
+    document.cookie = "age" + "=" + userAge.value;
+  }
 });
 
 console.log(document.cookie);
@@ -25,8 +27,9 @@ console.log(document.cookie);
 //------------------------------------------------
 
 const dateContainer = document.querySelector(".date__container");
-const createElem = elem => `${elem}`;
-const addElem = (elem, out = dateContainer) => out.insertAdjacentHTML('beforeend', elem); 
+const createElem = (elem) => `${elem}`;
+const addElem = (elem, out = dateContainer) =>
+out.insertAdjacentHTML("beforeend", elem);
 const app = createElem(`<div class="date__counter">
                             <h2>left until the birthday</h2>
                             <div class="date__wrapper">
@@ -68,67 +71,66 @@ let dateCounter = document.querySelector(".date__counter"),
     secondsOut = document.querySelector(".seconds__counter"),
     dateText = document.querySelector(".date__text");
 
-dateBirth.addEventListener('oninput', function() {
-	let userAnswer = dateBirth.value.split('-');
-
-    if(document.cookie.value == userAnswer) {
-        dateBirth.style.display = "none";
-    } else {
-        let userDate = {
-            year : userAnswer[0],
-            month : userAnswer[1],
-            date : userAnswer[2]
-        };
-    
-        document.cookie = 'month=' + userDate.month;
-        document.cookie = 'date=' + userDate.date;
-        document.cookie = 'year=' + userDate.year;
-    }
-});
-
-function getCookie(name) {
-	let matches = document.cookie.match(new RegExp(name.replace(/([.$?*+\\\/{}|()\[\]^])/g, '\\$1') + '=(.*?)(?:;|$)'));
-	return matches ? decodeURIComponent(matches[1]) : undefined;
+function getCookie() {
+    const COOKIE_KEY = "age";
+    const cookiesInArray = document.cookie.split("; ");
+    const targetCookie = cookiesInArray.find((cookie) => {
+    return cookie.startsWith(COOKIE_KEY);
+    });
+  
+    return targetCookie;
 }
-
-let date = new Date(); 
-//const deadline = new Date(getCookie('month'), getCookie('date'));
-let deadline = new Date(date.getFullYear() + 1, getCookie('month'), getCookie('date'), 0, 0, 0);
-//let timerId = null;    
-let id       = setInterval(countdownTimer, 1000, deadline, dateCounter);
-
-function countdownTimer() {
-    const now = new Date();
-    //const diff = deadline - now;
-    let diff = Math.ceil((deadline - now) / 1000);
-
-    if (diff <= 0) {
-        clearInterval(timerId);
-    } 
-
-    let dateNow  = new Date();
-
-    //let diff = Math.ceil((deadline - now) / 1000);
-    let seconds  = diff % 60,
-        minutes   = Math.floor(diff / 60) % 60,
-        hours    = Math.floor(diff / (60 * 60)) % 24,
-     
-        target   = Math.floor(diff / (60 * 60 * 24)), //Всего дней
-        days     = Math.floor(diff / (60 * 60 * 24) / target),
-        months  = Math.round(diff / (60 * 60 * 24) / 31);
-
-        minutes = (minutes < 10) ? '0' + minutes : minutes;
-        seconds = (seconds < 10) ? '0' + seconds : seconds;
-        hours = (hours < 10) ? '0' + hours : hours;
+  
+const dateHandler = (e) => {
+    document.cookie = `age=${e.target.value}`;
+    checkCookie();
+};
+  
+const checkCookie = () => {
+    const value = getCookie();
+    if (value) {
+        dateBirth.style.display = "none";
+        dateBirth.removeEventListener("change", dateHandler);
+        dateTimerStart();
+    } else {        
+        dateCounter.style.display = "none";  
+        dateBirth.addEventListener("change", dateHandler);
+    }
+};
+  
+checkCookie();
+  
+function dateTimerStart() {
+    const cookie = getCookie().split("=")[1];
+    console.log(cookie);
+    const currentYear = new Date().getFullYear();
+    const nowTime = Date.now();
+    const nextBirthday = new Date(cookie);
+    nextBirthday.setFullYear(currentYear); 
+    if (nowTime > nextBirthday.getTime()) {
+        nextBirthday.setFullYear(currentYear + 1);
+    }
+  
+    setInterval(() => {
+        let birthDate = new Date(cookie);
+        let month = birthDate.getMonth();
+        let day = birthDate.getDate();
+        let nextBirthDate = new Date(new Date().getFullYear() + 1, month, day);
+        
+        let diff = nextBirthDate - new Date();
+        let months = new Date(diff).getUTCMonth();
+        let days = new Date(diff).getUTCDate() - 1;
+        let hours = new Date(diff).getUTCHours();
+        let minutes = new Date(diff).getUTCMinutes();
+        let seconds = new Date(diff).getUTCSeconds();
 
     monthsOut.textContent = `${months}`;
     daysOut.textContent = `${days}`;
     hoursOut.textContent = `${hours}`;
     minutesOut.textContent = `${minutes}`;
     secondsOut.textContent = `${seconds}`;
-}   
-//countdownTimer();
-let timerId = setInterval(countdownTimer(),1000);
+    }, 1000);
+}
 
 //---------------------------------------------------------------------
 
@@ -136,11 +138,11 @@ let txtArea = document.querySelector(".text__area");
 const txtToStorage = localStorage.getItem("txtArea");
 
 if (txtToStorage) {
-	txtArea.value = txtToStorage;
+  txtArea.value = txtToStorage;
 }
 
 txtArea.oninput = () => {
-	localStorage.setItem("txtArea",txtArea.value);
+  localStorage.setItem("txtArea", txtArea.value);
 };
 
 //-----------------------------------------------------------------
@@ -148,8 +150,8 @@ txtArea.oninput = () => {
 const arr = [1, 2, 3, 4, 5];
 //Используя splice сделайте из него массив [1, 'a', 'b', 2, 3, 4, 'c', 5, 'e'].
 
-arr.splice(1, 0, 'a','b');
-arr.splice(6, 0, 'c');
-arr.splice(8, 0, 'e');
+arr.splice(1, 0, "a", "b");
+arr.splice(6, 0, "c");
+arr.splice(8, 0, "e");
 
 console.log(arr);
